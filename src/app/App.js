d6e802c5c53b06dad1604e9b81249d18bcfb35ff
Rosie Watson <rosie.watson@holidayextras.com>
2019-04-15 21:58:59 +0100

    Adjusting sidebar for mobile

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     return ( 
       <div className='main'>
         <Animation />
-        <div className='content position-absolute pt-3'>
+        < div className = 'content position-absolute pt-3 ml-3 ml-md-0' >
           <h1 className='text-center'>ROSIE WATSON</h1>
           <img src={Picture} className='d-block ml-auto mr-auto' />
           <h2 className='h3 text-white text-center p-2 m-3 block-xs'>Full-stack Web Developer</h2>
