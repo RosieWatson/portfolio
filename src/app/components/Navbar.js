31f02c07ebaa345c1e43203e18ae68aef0bdcfeb
Rosie Watson <rosie.watson@holidayextras.com>
2019-04-15 20:10:15 +0100

    Making the elements more responsive

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Navbar extends React.Component {
   render() {
     return (
-      <div className='navbar w-100 py-0 px-5'>
+      <div className='navbar w-100 py-0 px-4 px-md-5'>
         <a href=''>Portfolio</a>
         <a href=''>Experience</a>
         <a href=''>Eduction</a>
