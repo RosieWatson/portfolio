5ba8cd9e9adfa06126382f529f7c64e841a4ae4d
Rosie Watson <rosie.watson@holidayextras.com>
2020-06-04 14:29:39 +0100

    Slide redesign

--- src/app/App.js
+++ src/app/App.js
@@ -2,67 +2,55 @@ import React from 'react'
 import '../styles/styles'
 
 import Animation from './components/Animation'
+import Card from './components/Card'
 import Github from './components/Github'
 
 class App extends React.Component {
   constructor(props){
     super(props)
 
-    this.myDivToFocus = React.createRef()
+    this.experienceDiv = React.createRef()
+    this.educationDiv = React.createRef()
  }
 
-  handleOnClick = () => {
-    console.log('hitting this')
-    if(this.myDivToFocus.current){
-        this.myDivToFocus.current.scrollIntoView({ 
-           behavior: "smooth", 
-           block: "nearest"
-        })
+  handleOnClick = (div) => {
+    if(this[div].current){
+      this[div].current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest'
+      })
     }
-}
-
+  }
 
 
   render() {
     return (
       <div className='slide-container'>
         <section>
-          <div className='main align-items-center'>
+          <div className='main'>
             <Animation />
             <div className='pl-1 pl-md-5 m-4 w-100'>
               <h1 className='mb-3'>ROSIE WATSON</h1>
               <h4>Front-End Developer</h4>
             </div>
-            <button onClick={this.handleOnClick}><h1 className='mr-3'><i className='fas fa-arrow-circle-right'></i></h1></button>
+            <button onClick={() => this.handleOnClick('experienceDiv')}><h1><i className='fas fa-arrow-circle-right'></i></h1></button>
           </div>
         </section>
-        <section ref={this.myDivToFocus}>
+        <section ref={this.experienceDiv}>
+        <div className='main p-4'>
+            <Animation />
+            <div className='d-flex pl-1 pl-md-5 m-4 w-100 justify-content-center'>
+              <Card cardId={'experience'} />
+            </div>
+            <button onClick={() => this.handleOnClick('educationDiv')}><h1><i className='fas fa-arrow-circle-right'></i></h1></button>
+          </div>
+        </section>
+        <section ref={this.educationDiv}>
           <div className='main p-4'>
             <Animation />
-            <div className='d-flex flex-wrap align-items-center justify-content-around w-100'>
-              <div className='card m-5' style={{width: '18rem'}}>
-                <div className='card-body'>
-                  <h5 className='card-title'>Experience</h5>
-                  <p className='card-text'>3+ years of experience with JavaScript.</p>
-                </div>
-              </div>
-              <div className='card m-5' style={{width: '18rem'}}>
-                <div className='card-img-top text-center pt-3'>
-                  <i className='fas fa-user-graduate fa-4x'></i>
-                </div>
-                <div className='card-body'>
-                  <h5 className='card-title'>Education</h5>
-                  <p className='card-text'>University of Kent</p>
-                </div>
-              </div>
-              <div className='card m-5' style={{width: '18rem'}}>
-                <div className='card-body'>
-                  <h5 className='card-title'>Skills</h5>
-                  <p className='card-text'>React, Redux.</p>
-                </div>
+            <div className='d-flex pl-1 pl-md-5 m-4 w-100 justify-content-center'>
+              <Card cardId={'education'} />
             </div>
-            </div>
-            <button onClick={this.handleOnClick}><h1 className='mr-3'><i className='fas fa-arrow-circle-right'></i></h1></button>
           </div>
         </section>
         <Github />
---
+++ src/lib/sellingPoints.js
@@ -0,0 +1,46 @@
+export default {
+  education: {
+    id: 'education',
+    icon: 'fa-user-graduate',
+    title: 'Education',
+    data: [
+      {
+        title: 'University of Kent',
+        details: 'Got myself a first'
+      }
+    ]
+  },
+  experience: {
+    id: 'experience',
+    icon: 'fa-user-graduate',
+    title: 'Experience',
+    data: [
+      {
+        title: 'Holiday Extras',
+        details: 'So much time'
+      },
+      {
+        title: 'Apart of Me',
+        details: 'Volunteering pro'
+      }
+    ]
+  },
+  projects: {
+
+  },
+  skills: {
+    id: 'skills',
+    icon: 'fa-user-graduate',
+    title: 'Skills',
+    data: [
+      {
+        title: 'Languages',
+        details: 'JavaScript, HTML, CSS'
+      },
+      {
+        title: 'Other technologies',
+        details: 'React, Redux, etc'
+      }
+    ]
+  }
+}
