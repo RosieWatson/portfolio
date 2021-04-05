2be52ab3cb2eabf3c94b0f7b98c40bad8191cc98
Rosie Watson <rosiewatson@icloud.com>
2021-04-05 10:49:06 +0100

    Refining profile sidebar

--- src/app/components/ProfileSideBar.tsx
+++ src/app/components/ProfileSideBar.tsx
@@ -5,15 +5,26 @@ interface ProfileSideBarProps {
 }
 
 const ProfileSideBar = ({ setShowSideBar }: ProfileSideBarProps) => (
-  <div className='sideBar'>
-    <a href='mailto:rosiewatson@me.com' target='_blank' rel='noopener'>
-      <h1 className='mb-3'>
-        ROSIE WATSON
-        <i className='fas fa-envelope fa-xs' />
-      </h1>
-    </a>
-    <h4>Full Stack Developer</h4>
-    <button onClick={() => setShowSideBar(false)}>hide me</button>
+  <div className='side-bar-container'>
+    <div className='side-bar'>
+      <div>
+        <img
+          src={require('../../assets/profile.png')}
+          className='profile-photo'
+        />
+        <a href='mailto:rosiewatson@me.com' target='_blank' rel='noopener'>
+          <h1 className='mb-3'>
+            ROSIE WATSON
+            <i className='fas fa-envelope fa-xs' />
+          </h1>
+        </a>
+        <h4>Full Stack Developer</h4>
+      </div>
+      <button className='btn btn-primary' onClick={() => setShowSideBar(false)}>
+        Hide me
+        {/* Different text for mobile vs desktop? */}
+      </button>
+    </div>
   </div>
 )
 
